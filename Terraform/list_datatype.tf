e6a433c69ea8bb854c052863d6c8334b29cad72c
deepankarpatel <deepankarpatel15@gmail.com>
2024-05-16 16:33:32 +0530

    new file

--- Terraform/list_datatype.tf
+++ Terraform/list_datatype.tf
@@ -5,3 +5,7 @@ variable "my_list" {
 output "variable_value" {
   value = var.my_list
 }
+
+#this is a hello world program of list data type after running this program
+# it will ask you for value you have to enter the value in the form of list
+# for example ["hii", "hello", "how are you"] like this.
