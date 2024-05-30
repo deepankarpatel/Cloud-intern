940571601a7d55fe19be39a88bb21e5c4e985375
deepankarpatel <deepankarpatel15@gmail.com>
2024-05-30 12:31:34 +0530

    new file

---
+++ Terraform/lifecycle_meta_argument.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "Myrg" {
+  name     = "MetaArgument"
+  location = "central india"
+}
+
+resource "azurerm_lb" "Load" {
+  name                = "MyLB"
+  resource_group_name = azurerm_resource_group.Myrg.name
+  location            = azurerm_resource_group.Myrg.location
+
+
+  tags = {
+    name = "Hello world"
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+#this code is ignoring the manually added change to the loadbalancer with meta argument lyfecycle "ignore changes"
+
