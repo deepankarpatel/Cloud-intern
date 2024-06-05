4367c8ed9b212acb6409d67ae4a11df4abf800a6
deepankarpatel <deepankarpatel15@gmail.com>
2024-06-05 12:40:53 +0530

    new file added

---
+++ Terraform/Provisioners/remote_exac.tf
@@ -0,0 +1,132 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "myrg" {
+  name     = "sunday"
+  location = "central india"
+}
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+}
+
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "internal-subnet"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "myip" {
+  name                = "internetIP"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "interface_2" {
+  name                 = "nic"
+  location             = azurerm_resource_group.myrg.location
+  resource_group_name  = azurerm_resource_group.myrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myip.id
+  }
+}
+
+resource "azurerm_network_security_group" "mynsg" {
+  name                = "security_group"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "HTTP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "attatchment-2" {
+  network_interface_id      = azurerm_network_interface.interface_2.id
+  network_security_group_id = azurerm_network_security_group.mynsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "myvm" {
+  name                = "veronica"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  size                = "Standard_F2"
+  admin_username      = "cloud"
+  admin_password      = "cloud@1234567"
+  network_interface_ids = [
+    azurerm_network_interface.interface_2.id,
+  ]
+
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "cloud"
+      password = "cloud@1234567"
+      host     = self.public_ip_address
+    }
+
+  }
+}
+
+
+#this code will create a linux vm and connect to it automatically and intall ngnix server on this vm
