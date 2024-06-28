286e275e8f6671c7ecdf667987c33d1f960eeabb
deepankarpatel <deepankarpatel15@gmail.com>
2024-06-28 12:43:06 +0530

    updated

--- Terraform/list_datatype.tf
+++ Terraform/list_datatype.tf
@@ -1,11 +1,11 @@
 variable "my_list" {
     type = list
+    default = ["game", "name", "me", "know"]
 }
 
 output "variable_value" {
   value = var.my_list
 }
-
 #this is a hello world program of list data type after running this program
 # it will ask you for value you have to enter the value in the form of list
 # for example ["hii", "hello", "how are you"] like this. so it will give you output
--- Terraform/map_datatype.tf
+++ Terraform/map_datatype.tf
@@ -1,11 +1,13 @@
 variable "my_map" {
   type = map
+  default = {"name" = "Deepankar", "age" = "23", "team" = "azure" }
 }
+
+
 
 output "map_output" {
   value = var.my_map
 }
-
 #this is a hello world program of map data type after running this program
 # it will ask you for value you have to enter the value in the form of map
 # for example {"red"="white", "name"= "value"} like this. so it will give you output
